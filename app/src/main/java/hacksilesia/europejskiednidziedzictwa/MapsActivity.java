9ef41aa629f715f006e4487fa0b5222835a60be0
Angelika Serwa <graszka223@gmail.com>
2016-02-20 01:58:18 +0100

    build

--- app/src/main/java/hacksilesia/europejskiednidziedzictwa/MapsActivity.java
+++ app/src/main/java/hacksilesia/europejskiednidziedzictwa/MapsActivity.java
@@ -1,7 +1,10 @@
 package hacksilesia.europejskiednidziedzictwa;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,10 +21,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 

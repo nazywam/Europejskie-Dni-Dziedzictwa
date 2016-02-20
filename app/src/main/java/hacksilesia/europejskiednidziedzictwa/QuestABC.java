d54ae3721672632fe29c77a519e49d8861c271af
Angelika Serwa <graszka223@gmail.com>
2016-02-20 03:52:15 +0100

    asdg

--- app/src/main/java/hacksilesia/europejskiednidziedzictwa/QuestABC.java
+++ app/src/main/java/hacksilesia/europejskiednidziedzictwa/QuestABC.java
@@ -8,12 +8,37 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
-public class QuestABC extends AppCompatActivity {
+import com.kontakt.sdk.android.ble.configuration.ActivityCheckConfiguration;
+import com.kontakt.sdk.android.ble.configuration.ForceScanConfiguration;
+import com.kontakt.sdk.android.ble.configuration.ScanPeriod;
+import com.kontakt.sdk.android.ble.configuration.scan.EddystoneScanContext;
+import com.kontakt.sdk.android.ble.configuration.scan.IBeaconScanContext;
+import com.kontakt.sdk.android.ble.configuration.scan.ScanContext;
+import com.kontakt.sdk.android.ble.connection.OnServiceReadyListener;
+import com.kontakt.sdk.android.ble.discovery.BluetoothDeviceEvent;
+import com.kontakt.sdk.android.ble.manager.ProximityManager;
+import com.kontakt.sdk.android.ble.manager.ProximityManagerContract;
+import com.kontakt.sdk.android.common.*;
+import com.kontakt.sdk.android.common.log.LogLevel;
+import com.kontakt.sdk.android.common.profile.DeviceProfile;
+import com.kontakt.sdk.android.common.profile.RemoteBluetoothDevice;
+import com.kontakt.sdk.android.manager.KontaktProximityManager;
 
+import java.util.List;
+
+public class QuestABC extends AppCompatActivity implements ProximityManager.ProximityListener {
+    private static final String TAG = "BEACONY";
+    private ProximityManagerContract proximityManager;
+    private ScanContext scanContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        KontaktSDK.initialize(this)
+                .setDebugLoggingEnabled(com.kontakt.sdk.android.common.BuildConfig.DEBUG)
+                .setLogLevelEnabled(LogLevel.DEBUG, true);
         setContentView(R.layout.activity_quest_abc);
+        KontaktSDK.initialize("RGfcELVJXbOBOnjBuuxRMKeflkEyqEfs");
+        proximityManager = new KontaktProximityManager(this);
     }
 
     public void onGood(View view)
@@ -24,6 +49,81 @@ public class QuestABC extends AppCompatActivity {
     public void onBad(View view)
     {
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        proximityManager.initializeScan(getScanContext(), new OnServiceReadyListener() {
+            @Override
+            public void onServiceReady() {
+                proximityManager.attachListener(QuestABC.this);
+            }
+
+            @Override
+            public void onConnectionFailure() {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        proximityManager.detachListener(this);
+        proximityManager.disconnect();
+    }
+
+    private ScanContext getScanContext() {
+        if (scanContext == null) {
+            scanContext = new ScanContext.Builder()
+                    .setScanPeriod(ScanPeriod.RANGING) // or for monitoring for 15 seconds scan and 10 seconds waiting:
+                            //.setScanPeriod(new ScanPeriod(TimeUnit.SECONDS.toMillis(15), TimeUnit.SECONDS.toMillis(10)))
+                    .setScanMode(ProximityManager.SCAN_MODE_BALANCED)
+                    .setActivityCheckConfiguration(ActivityCheckConfiguration.MINIMAL)
+                    .setForceScanConfiguration(ForceScanConfiguration.MINIMAL)
+                    .setIBeaconScanContext(new IBeaconScanContext.Builder().build())
+                    .setEddystoneScanContext(new EddystoneScanContext.Builder().build())
+                    .setForceScanConfiguration(ForceScanConfiguration.MINIMAL)
+                    .build();
+        }
+        return scanContext;
+    }
+
+    @Override
+    public void onEvent(BluetoothDeviceEvent bluetoothDeviceEvent) {
+        List<? extends RemoteBluetoothDevice> deviceList = bluetoothDeviceEvent.getDeviceList();
+        long timestamp = bluetoothDeviceEvent.getTimestamp();
+        DeviceProfile deviceProfile = bluetoothDeviceEvent.getDeviceProfile();
+        switch (bluetoothDeviceEvent.getEventType()) {
+            case SPACE_ENTERED:
+                Log.d(TAG, "namespace or region entered");
+                break;
+            case DEVICE_DISCOVERED:
+                Log.d(TAG, "found new beacon");
+                break;
+            case DEVICES_UPDATE:
+                Log.d(TAG, "updated beacons");
+                //to powinnismy sprawdzic czy to odpowiednii beacon i wysunąc menu
+                break;
+            case DEVICE_LOST:
+                Log.d(TAG, "lost device");
+                break;
+            case SPACE_ABANDONED:
+                //opuszczamy nasz region schowaj menu
+                Log.d(TAG, "namespace or region abandoned");
+                break;
+        }
+    }
+
+    @Override
+    public void onScanStart() {
+        Log.d(TAG, "scan started");
+    }
+
+    @Override
+    public void onScanStop() {
+        Log.d(TAG, "scan stopped");
     }
 
 }
